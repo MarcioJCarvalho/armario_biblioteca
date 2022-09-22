73bf2441ca3fc2b78e5732ec16bde1d9f110a1ee
Aluno <aluno@email.com>
2022-09-21 22:31:05 -0300

    correções do maven

--- armario_biblioteca/src/main/java/br/com/mjc/Main.java
+++ armario_biblioteca/src/main/java/br/com/mjc/Main.java
@@ -1,16 +1,26 @@
 package br.com.mjc;
 
+import br.com.mjc.connection.HibernateUtil;
 import br.com.mjc.model.Emprestimo;
 import br.com.mjc.service.EmprestimoService;
 import br.com.mjc.view.MainMenuUI;
+import org.hibernate.Session;
 
 public class Main {
 
     public static void main(String[] args) {
-        MainMenuUI mainMenuUI = new MainMenuUI();
-        mainMenuUI.setVisible(true);
+//        MainMenuUI mainMenuUI = new MainMenuUI();
+//        mainMenuUI.setVisible(true);
+        
+        
+
+        Session sessao = HibernateUtil.getSession();
+        sessao.beginTransaction();
         Emprestimo emprestimo = new Emprestimo();
         EmprestimoService.emprestar(emprestimo);
+        sessao.save(emprestimo);
+        sessao.getTransaction().commit();
+        HibernateUtil.encerraSession();
 
     }
 }
